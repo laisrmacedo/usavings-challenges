cf06ba1ae5db496054ba92f4a3f53af9a6d7af2e
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-09-08 22:46:38 +0200

    refactor: delete files

--- inventory-backend/src/goods-in/goods-in.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('goods-in')
-export class GoodsInController {}
--- inventory-backend/src/goods-out/goods-out.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('goods-out')
-export class GoodsOutController {}
