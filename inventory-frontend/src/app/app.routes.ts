b7ff2ce68bd179185bfdfcfd35282b7c0921988b
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-09-08 08:23:16 +0200

    refactor: component name changed

--- inventory-frontend/src/app/app.routes.ts
+++ inventory-frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 import { OverviewPageComponent } from './pages/overview-page/overview-page.component';
-import { ManagementPageComponent } from './management-page/management-page.component';
+import { ManagementPageComponent } from './pages/management-page/management-page.component';
 
 export const routes: Routes = [
   { path: 'produtos', component: ProductsPageComponent },
