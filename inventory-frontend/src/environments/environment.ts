b17f6213d44b175fa7a872f327c25dec1d94960d
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-09-11 21:25:39 +0200

    fix: vercel deploy

--- inventory-frontend/src/environments/environment.ts
+++ inventory-frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://usavings-challenges.onrender.com'
+  // apiUrl: 'https://usavings-challenges.onrender.com'
+  apiUrl: 'http://localhost:3000'
 };
